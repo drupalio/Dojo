73599454d8d926f2e66703b5fc1c27596a7b81e8
Drupalio <drupalio@Evilness>
2017-06-30 16:27:27 -0500

    prueba par datos de un formulario

---
+++ Php/4/index.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST['submit'])) 
+{ 
+    $name = $_POST['nombre'];
+    echo "Hola ".name." eres el papá de los helados"; 
+}
+?>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+   <input type="text" name="nombre"><br>
+   <input type="submit" name="submit" value="Submit Form"><br>
+</form>
