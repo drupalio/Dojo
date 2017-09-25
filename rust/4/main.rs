42aaafdeb3d3c97e344c76da45ba15ab5522cbdb
Ricardo Morales Paredes <pntra2.team@gmail.com>
2017-09-25 13:15:56 -0500

    creating custom functions in rust

---
+++ rust/4/main.rs
@@ -0,0 +1,12 @@
+fn sentido_de_la_vida() -> String {
+    "Hacerla chillar".to_string()
+}
+
+fn cuando_inicia_senectud() -> i32{
+    27
+}
+
+fn main(){
+    println!("El sentido de la vida es {}",sentido_de_la_vida());
+    println!("La senectud inicia {}",cuando_inicia_senectud());
+}
